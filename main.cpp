1aff0ab9bbb9e234d206038925f6536ff5968f0d
yesiot <goslawski.p@gmail.com>
2019-02-11 20:32:04 +0100

    Improve add version script

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(const int argc, const char* argv[]) {
 
-    std::cout << "My version: " << VERSION << std::endl;
-
+    std::cout << "Revision: " << REVISION << std::endl;
+    std::cout << "Version: " << VERSION << std::endl;
     return 0;
 }
